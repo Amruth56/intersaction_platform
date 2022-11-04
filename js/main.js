f3e1e390d0d9b788b4190c2be0907bb2b5565b8d
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2022-11-04 07:52:23 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//we are defining the connection from the client side to the socket.
+
+const socket = io('/'); // as the port that we are using id under the sam directory we can use '/' symbol to fetch it 
+
+/*
+const socket = io('localhost:3000')  // we tell our socket io that socket server is found in 'localhost:3000'
+*/
+
+
+
+// socket.on --> Set up callback functions for various events on the WebSocket connection.
+
+// 1st we try to connect to the server and if the connection is a success then we would like to console successfully connected to web socket server 
+socket.on('connect', () => {
+    console.log('successfully connected to socket.io server ');
+    console.log(socket.id);
+})
