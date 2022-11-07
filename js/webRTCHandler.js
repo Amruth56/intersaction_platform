2112b43007b5dd5a29dae31587be44460d215176
Amruth <ammumandappa@gmail.com>
2022-11-08 00:01:49 +0530

    .

---
+++ js/webRTCHandler.js
@@ -0,0 +1,53 @@
+import * as wss from "./wss.js";
+import * as constants from "./constants.js";
+import * as ui from "./ui.js";
+
+let connectedUserDetails;
+
+export const sendPreOffer = (callType, calleePersonalCode) => {
+    connectedUserDetails = {
+        callType,
+        socketId: calleePersonalCode,
+    };
+
+    if (
+        callType === constants.callType.CHAT_PERSONAL_CODE ||
+        callType === constants.callType.VIDEO_PERSONAL_CODE
+    ) {
+        const data = {
+            callType,
+            calleePersonalCode,
+        };
+        ui.showCallingDialog(callingDialogRejectCallHandler);
+        wss.sendPreOffer(data);
+    }
+};
+
+export const handlePreOffer = (data) => {
+    const { callType, callerSocketId } = data;
+
+    connectedUserDetails = {
+        socketId: callerSocketId,
+        callType,
+    };
+
+    if (
+        callType === constants.callType.CHAT_PERSONAL_CODE ||
+        callType === constants.callType.VIDEO_PERSONAL_CODE
+    ) {
+        console.log("showing call dialog");
+        ui.showIncomingCallDialog(callType, acceptCallHandler, rejectCallHandler);
+    }
+};
+
+const acceptCallHandler = () => {
+    console.log("call accepted");
+};
+
+const rejectCallHandler = () => {
+    console.log("call rejected");
+};
+
+const callingDialogRejectCallHandler = () => {
+    console.log("rejecting the call");
+};
