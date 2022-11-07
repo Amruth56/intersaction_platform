3093bf9cf488c829d85276e924ea31fce918dc1d
Amruth <ammumandappa@gmail.com>
2022-11-07 23:31:36 +0530

    .

---
+++ js/store.js
@@ -0,0 +1,51 @@
+let state = {
+    socketId: null,
+    localStream: null,
+    remoteStream: null,
+    screenSharingStream: null,
+    allowConnectionsFromStrangers: false,
+    screenSharingActive: false,
+
+};
+
+export const sectSocketId = (socketId) => {
+    state = {
+        ...state,
+        socketId
+    };
+};
+
+export const setLocalStream = (stream) => {
+    state = {
+        ...state,
+        localStream: stream
+    }
+}
+
+export const setAllowConnectionsFromStrangers = (allowConnection) => {
+    state = {
+        ...state,
+        allowConnectionsFromStrangers: allowConnection
+    }
+}
+
+export const setScreenSharingActive = (screenSharingActive) => {
+    state = {
+        ...state,
+        screenSharingActive,
+    };
+};
+
+export const setScreenSharingStream = (stream) => {
+    state = {
+        ...state,
+        screenSharingActive: stream,
+    };
+};
+
+export const setRemoteStream = (stream) => {
+    state = {
+        ...state,
+        remoteStream: stream,
+    };
+};
